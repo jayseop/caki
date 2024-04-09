160096f822ece9355bf5015f9b4e15e0651f0653
justin <ggm324@naver.com>
2024-04-09 14:47:03 +0900

    First Upload

---
+++ caki_app/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class CakiAppConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'caki_app'
---
+++ caki_app/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from .import views
+
+app_name = 'myapp'
+
+urlpatterns = [
+    #path('',views.main, name='main') #메인 페이지 url
+    path('', views.user_login, name='login'), # 일단 로그인 url, 메인 url로 설정 추후 변경 예정
+    path('signup/', views.user_signup, name='signup') # 회원가입 페이지 url
+    
+]
