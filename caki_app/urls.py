088da52174a00f858991961e46efb68cb50632e3
justin <ggm324@naver.com>
2024-04-15 04:29:26 +0900

    어플 메인 페이지 임시 url

--- caki_app/urls.py
+++ caki_app/urls.py
@@ -5,8 +5,7 @@ from .import views
 app_name = 'myapp'
 
 urlpatterns = [
-    #path('',views.main, name='main') #메인 페이지 url
-    path('', views.user_login, name='login'), # 일단 로그인 url, 메인 url로 설정 추후 변경 예정
-    path('signup/', views.user_signup, name='signup') # 회원가입 페이지 url
-    
+    path('',views.main_page, name='main'), #메인 페이지 url
+    path('login/', views.user_login, name='login'), # 일단 로그인 url, 메인 url로 설정 추후 변경 예정
+    path('signup/', views.user_signup, name='signup') # 회원가입 페이지 url 
 ]
