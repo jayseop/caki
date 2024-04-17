fb63fd60f1d940f8f741b11da0df91d9841170a0
skxcv312 <skxcv312@naver.com>
2024-04-17 15:28:41 +0900

    로그인 유지, userAPI변경

--- caki_app/APIView/user_api_views.py
+++ caki_app/APIView/user_api_views.py
@@ -1,19 +1,73 @@
 from django.contrib.auth import authenticate
-from django.contrib.auth.models import User
-from django.shortcuts import render
+from django.shortcuts import get_object_or_404
 from django.http import HttpResponse
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework_simplejwt.serializers import TokenObtainPairSerializer, TokenRefreshSerializer
 from rest_framework.views import APIView
+from caki_app.models import *
 from caki_app.serializers import *
 from mysite.settings import SECRET_KEY
+import jwt
 
-# def main_page(request):
-#     return render(request, 'main.html')
+# 다른 페이지 로드시 로그인 유지
+class OnlyUserView(APIView) :
+  def get(self,request): 
+        try:
+            # access token을 decode 해서 유저 id 추출 => 유저 식별
+            access_token = request.COOKIES['access']
+            payload = jwt.decode(access_token, SECRET_KEY, algorithms=['HS256']) # access 토큰으로 유저 확인
+            pk = payload.get('user_id') # pk = idMember
+            user = get_object_or_404(Member, pk=pk) # 데이터 베이스에서 유저 정보 추출
+            serializer = UserSerializer(instance=user)
+            res = Response({
+                    'user' : serializer.data 
+                    },status=status.HTTP_200_OK)
+            
+            return res
+        
+        # access 토큰 만료 시 토큰 갱신
+        except(jwt.exceptions.ExpiredSignatureError):
+            data = {'refresh': request.COOKIES.get('refresh', None)} # refrash 토큰 추출
+            serializer = TokenRefreshSerializer(data=data) # refrash 토큰으로 access토큰 추출
+            if serializer.is_valid(raise_exception=True): 
+                access_token = serializer.validated_data.get('access', None) # access 토큰 재발급
+                # refresh_token = serializer.validated_data.get('refresh', None) # refrash 토큰 재발급
+
+                payload = jwt.decode(access_token, SECRET_KEY, algorithms=['HS256']) # access 토큰으로 유저 확인
+                pk = payload.get('user_id') # pk = idMember
+                user = get_object_or_404(Member, pk=pk) # 데이터 베이스에서 유저 정보 추출
+
+                serializer = UserSerializer(instance=user)
+                res = Response({
+                    'user' : serializer.data,
+                    'server' : 'access_token_renewal',
+                    },status=status.HTTP_200_OK)
+                
+                res.set_cookie('access', access_token) 
+                # res.set_cookie('refresh', refresh_token)
+                return res
+            raise jwt.exceptions.InvalidTokenError
+
+        # refrash 토큰까지 만료
+        except(jwt.exceptions.InvalidTokenError):
+            return Response({"server": "refrash_token_None",}) # 로그인 페이지
+
+
+# 회원가입 test용
+# {
+# 	"email": "skxcv312@naver.com",
+#     "password":"1234",
+#     "nickname":"jaja",
+#     "qual": "on",
+#     "introduce":"hi" 
+# }
 
 #회원가입
 class SignupAPIView(APIView):
+    def get(self, request):
+        return Response(request) # 회원가입 페이지
+    
     def post(self, request):
         print(request.data)
         serializer = UserSerializer(data=request.data)
@@ -28,11 +82,7 @@ class SignupAPIView(APIView):
             res = Response(
                 {
                     "user": serializer.data,
-                    "message": "signup successs",
-                    "token": {
-                        "access": access_token,
-                        "refresh": refresh_token,
-                    },
+                    "server": "signup_successs",
                 },
                 status=status.HTTP_200_OK,
             )
@@ -41,56 +91,24 @@ class SignupAPIView(APIView):
             res.set_cookie("access", access_token, httponly=True)
             res.set_cookie("refresh", refresh_token, httponly=True)
             
-            return HttpResponse('signup successs') #회원가입 성공시 페이지
+            return res #회원가입 성공시 페이지
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
     
-    def get(self, request):
-        return render(request,'signup.html')
 
 
+# 로그인
 class AuthUserAPIView(APIView):
-    # 유저 정보 확인
-    def get(self, request):
-        # try:
-        #     # access token을 decode 해서 유저 id 추출 => 유저 식별
-        #     access = request.COOKIES['access']
-        #     payload = jwt.decode(access, SECRET_KEY, algorithms=['HS256'])
-        #     pk = payload.get('user_id')
-        #     user = get_object_or_404(User, pk=pk)
-        #     serializer = UserSerializer(instance=user)
-        #     return HttpResponse(serializer.data, status=status.HTTP_200_OK)
-
-        # except(jwt.exceptions.ExpiredSignatureError):
-        #     # 토큰 만료 시 토큰 갱신
-        #     data = {'refresh': request.COOKIES.get('refresh', None)}
-        #     serializer = TokenRefreshSerializer(data=data)
-        #     if serializer.is_valid(raise_exception=True):
-        #         access = serializer.data.get('access', None)
-        #         refresh = serializer.data.get('refresh', None)
-        #         payload = jwt.decode(access, SECRET_KEY, algorithms=['HS256'])
-        #         pk = payload.get('user_id')
-        #         user = get_object_or_404(User, pk=pk)
-        #         serializer = UserSerializer(instance=user)
-        #         res = Response(serializer.data, status=status.HTTP_200_OK)
-        #         res.set_cookie('access', access)
-        #         res.set_cookie('refresh', refresh)
-        #         return HttpResponse(res)
-        #     raise jwt.exceptions.InvalidTokenError
-
-        # except(jwt.exceptions.InvalidTokenError):
-        #     # 사용 불가능한 토큰일 때
-        #     return Response(status=status.HTTP_400_BAD_REQUEST)
- 
-        
-        return render(request,'login.html')
+    def get(self, request):        
+        return Response(request) #로그인 페이지
 
     # 로그인
     def post(self, request):
     	# 유저 인증
         user = authenticate(
-            email=request.data.get("email"), 
-            password=request.data.get("password")
+            email=request.data["email"], 
+            password=request.data["password"],
         )
+
         # 이미 회원가입 된 유저일 때
         if user is not None:
             serializer = UserSerializer(user)
@@ -101,27 +119,25 @@ class AuthUserAPIView(APIView):
             res = Response(
                 {
                     "user": serializer.data,
-                    "message": "login success",
-                    "token": {
-                        "access": access_token,
-                        "refresh": refresh_token,
-                    },
+                    "server": "Login_success",
                 },
                 status=status.HTTP_200_OK,
             )
             # jwt 토큰 => 쿠키에 저장
             res.set_cookie("access", access_token, httponly=True)
             res.set_cookie("refresh", refresh_token, httponly=True)
-            return HttpResponse('login successs')
+            return res
         else:
             return Response(status=status.HTTP_400_BAD_REQUEST)
 
     # 로그아웃
-    def delete(self, request):
+    def delete(self):
         # 쿠키에 저장된 토큰 삭제
-        response = Response({
-            "message": "Logout success"
+        res = Response({
+            "server": "Logout_success"
             }, status=status.HTTP_202_ACCEPTED)
-        response.delete_cookie("access")
-        response.delete_cookie("refresh")
-        return HttpResponse(response)
+        
+        res.delete_cookie("access")
+        res.delete_cookie("refresh")
+        return res
+    
